5593d99ce5d6811190781b8567f1dd3ea57377be
laculturaltelefonica-ship-it <laculturaltelefonica@gmail.com>
2025-12-17 21:32:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log("Web de Catering Gil & Román cargada correctamente.");
 
+// Scroll suave con botón
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Navbar cambia al hacer scroll
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
